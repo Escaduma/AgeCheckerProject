6baa2b58a061b3b6db89899d245ea74bfcace57b
Macie <mescaleraduron@csi.edu>
2026-04-16 14:33:28 -0600

    I change the range of the ages and says different things about your age

--- age_checker.py
+++ age_checker.py
@@ -1,10 +1,14 @@
 def check_age(age):
     if age <0:
         raise ValueError("Age cannot be negative")
-    if age>=18:
-        return "You are an adult"
+    if age>18 and age<60:
+        return "Regular"
+    if age<18:
+        return "Child"
+    if age>60 and age<=100:
+        return "Senior"
     else:
-        return "You are a minor"
+        return "You are from another centuary!!!! :o"
     
 # 1. Because it needs to tell what is wrong
 # 2. Becaue it is easy to find and to work with when mistakes are made.
